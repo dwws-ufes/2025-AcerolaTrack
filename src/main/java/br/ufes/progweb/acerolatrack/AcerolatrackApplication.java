973748b15056396c8a3c36b84991f0c487d997fa
bvl0 <icaro1561@outlook.com>
2025-06-02 21:33:26 -0300

    aaaaaaaaaaaaaa

---
+++ src/main/java/br/ufes/progweb/acerolatrack/AcerolatrackApplication.java
@@ -0,0 +1,13 @@
+package br.ufes.progweb.acerolatrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcerolatrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcerolatrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufes/progweb/acerolatrack/AcerolatrackApplicationTests.java
@@ -0,0 +1,15 @@
+package br.ufes.progweb.acerolatrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class AcerolatrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
