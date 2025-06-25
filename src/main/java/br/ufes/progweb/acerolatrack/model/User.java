c3928266ecbec40686f7951bbcdcaef46282afcb
bvl0 <icaro1561@outlook.com>
2025-06-25 08:10:39 -0300

    feat: primeiro endpoint funcionando

--- src/main/java/br/ufes/progweb/acerolatrack/model/User.java
+++ src/main/java/br/ufes/progweb/acerolatrack/model/User.java
@@ -3,9 +3,11 @@ package br.ufes.progweb.acerolatrack.model;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
 
 @Data
+@MappedSuperclass
 public abstract class User {
 
     private String username;
