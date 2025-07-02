d550b8f6eb857bdb099585a1b676953a558a1cdb
bvl0 <icaro1561@outlook.com>
2025-07-01 23:27:11 -0300

    fix: ajustando logica de role do worker

--- src/main/java/br/ufes/progweb/acerolatrack/core/repository/ManagerRepository.java
+++
@@ -1,7 +0,0 @@
-package br.ufes.progweb.acerolatrack.core.repository;
-
-import br.ufes.progweb.acerolatrack.model.Manager;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ManagerRepository extends JpaRepository<Manager, Long> {
-}
